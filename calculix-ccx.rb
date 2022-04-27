541f89e6078aad0eb584389b263739aede7a722d
Rob McDonald <rob.a.mcdonald@gmail.com>
2022-04-26 22:39:52 -0700

    Update to 2.20

--- calculix-ccx.rb
+++ calculix-ccx.rb
@@ -1,9 +1,9 @@
 class CalculixCcx < Formula
   desc "Three-Dimensional Finite Element Solver"
   homepage "http://www.calculix.de/"
-  url "http://www.dhondt.de/ccx_2.18.src.tar.bz2"
-  version "2.18"
-  sha256 "fad533bd66693daa398856262bf7c6feb12599c3051955238b0a70420852ff65"
+  url "http://www.dhondt.de/ccx_2.20.src.tar.bz2"
+  version "2.20"
+  sha256 "63bf6ea09e7edcae93e0145b1bb0579ea7ae82e046f6075a27c8145b72761bcf"
 
   livecheck do
     url :url
@@ -14,15 +14,15 @@ class CalculixCcx < Formula
   depends_on "gcc" if OS.mac? # for gfortran
 
   resource "test" do
-    version "2.18"
+    version "2.20"
     url "http://www.dhondt.de/ccx_#{version}.test.tar.bz2"
-    sha256 "c5f771fc152d876366570b0d88032d908d912efb566ccddf070184acceeed7f4"
+    sha256 "79848d88dd1e51839d1aed68fb547ff12ad3202c3561c02c2f3a8ceda0f2eb82"
   end
 
   resource "doc" do
-    version "2.18"
+    version "2.20"
     url "http://www.dhondt.de/ccx_#{version}.htm.tar.bz2"
-    sha256 "5b9cc5e6a1ef70bd93737b507cb754d485b229ec3cef31ea33501a74deff23e6"
+    sha256 "51a0922f5cecc9fbe5880afb47c3b24ef90300fb800b4d10fb02b297e7c2b4c1"
   end
 
   resource "spooles" do
@@ -82,34 +82,23 @@ class CalculixCcx < Formula
 end
 
 __END__
-diff --git a/ccx_2.18/src/CalculiX.h b/ccx_2.18/src/CalculiX.h
-index c0f5e88..1ba6f5d 100644
---- a/ccx_2.18/src/CalculiX.h
-+++ b/ccx_2.18/src/CalculiX.h
-@@ -320,6 +320,10 @@ void FORTRAN(autocovmatrix,(double *co,double *ad,double *au,ITG *jqs,
- 			    ITG *irows,ITG *ndesi,ITG *nodedesi,double *corrlen,
- 			    double *randomval,ITG *irobustdesign));
-
-+void FORTRAN(auglag_inclusion ,(int conttype, double *gcontfull, int nacti,
-+            int ncdim, double mufric, double atol, double rtol, double *pkvec,
-+            int kitermax, double timek ));
-+
- void FORTRAN(basis,(double *x,double *y,double *z,double *xo,double *yo,
-                     double *zo,ITG *nx,ITG *ny,ITG *nz,double *planfa,
-                     ITG *ifatet,ITG *nktet,ITG *netet,double *field,
-@@ -901,7 +905,7 @@ void FORTRAN(checktime,(ITG *itpamp,ITG *namta,double *tinc,double *ttime,
+diff --git a/ccx_2.20/src/CalculiX.h b/ccx_2.20/src/CalculiX.h
+index d01cfe17..7d30b7e2 100644
+--- a/ccx_2.20/src/CalculiX.h
++++ b/ccx_2.20/src/CalculiX.h
+@@ -761,7 +765,7 @@ void FORTRAN(checktime,(ITG *itpamp,ITG *namta,double *tinc,double *ttime,
  void FORTRAN(checktruecontact,(ITG *ntie,char *tieset,double *tietol,
               double *elcon,ITG *itruecontact,ITG *ncmat_,ITG *ntmat_));
-
+ 
 -void FORTRAN(clonesensitivies,(ITG *nobject,ITG *nk,char *objectset,
 +void FORTRAN(clonesensitivities,(ITG *nobject,ITG *nk,char *objectset,
  			       double *g0,double *dgdxglob));
-
+ 
  void FORTRAN(closefile,());
-@@ -1391,6 +1395,11 @@ void FORTRAN(detectactivecont1,(double *vold,ITG *nk,ITG *mi,double *aubi,
- 				ITG *jqib,double *g,ITG *icolbb,ITG *nactdof,
- 				double *qtmp));
-
+@@ -1181,6 +1185,11 @@ void FORTRAN(detectactivecont,(double *gapnorm,double *gapdisp,double *auw,
+ 			       ITG *iroww,ITG *jqw,ITG *nslavs,
+ 			       double *springarea,ITG *iacti,ITG *nacti));
+ 
 +void FORTRAN(detectactivecont2,(double *gapnorm, double *gapdof,
 +                double *auw, int *iroww, int *jqw,
 +                int neqtot, int nslavs, double *springarea,
@@ -118,19 +107,10 @@ index c0f5e88..1ba6f5d 100644
  void FORTRAN(determineextern,(ITG *ifac,ITG *itetfa,ITG *iedg,ITG *ipoed,
                                ITG *iexternedg,ITG *iexternfa,ITG *iexternnode,
                                ITG *nktet_,ITG *ipofa));
-@@ -4674,6 +4683,8 @@ void FORTRAN(reinit_refine,(ITG *kontet,ITG *ifac,ITG *ieln,ITG *netet_,
-                       ITG *newsize,ITG *ifatet,ITG *itetfa,ITG *iedg,
-                       ITG *ieled));
-
-+void FORTRAN(relaxval_al, (double *gcontfull, int nacti, int ncdim));
-+
- void remastruct(ITG *ipompc,double **coefmpcp,ITG **nodempcp,ITG *nmpc,
- 		ITG *mpcfree,ITG *nodeboun,ITG *ndirboun,ITG *nboun,
- 		ITG *ikmpc,ITG *ilmpc,ITG *ikboun,ITG *ilboun,
-@@ -6020,6 +6031,12 @@ void *u_realloc(void* num,size_t size,const char *file,const int line,const char
-
+@@ -4959,6 +4968,12 @@ void *u_realloc(void* num,size_t size,const char *file,const int line,const char
+ 
  void utempread(double *t1,ITG *istep,char *jobnamec);
-
+ 
 +double v_betrag(double *a);
 +
 +void v_prod( double *A, double *B, double *C );
@@ -140,56 +120,56 @@ index c0f5e88..1ba6f5d 100644
  void FORTRAN(varsmooth,(double *aub,double *adl,
  			     double *sol,double *aux,ITG *irow,
  			     ITG *jq,ITG *neqa,ITG *neqb,double *alpha));
-diff --git a/ccx_2.18/src/Makefile b/ccx_2.18/src/Makefile
-index b32a52e..a95e611 100755
---- a/ccx_2.18/src/Makefile
-+++ b/ccx_2.18/src/Makefile
+diff --git a/ccx_2.20/src/Makefile b/ccx_2.20/src/Makefile
+index d357f801..acea8218 100755
+--- a/ccx_2.20/src/Makefile
++++ b/ccx_2.20/src/Makefile
 @@ -25,7 +25,7 @@ LIBS = \
  	../../../ARPACK/libarpack_INTEL.a \
         -lpthread -lm -lc
-
--ccx_2.18: $(OCCXMAIN) ccx_2.18.a  $(LIBS)
-+ccx_2.18: $(OCCXMAIN) ccx_2.18.a
- 	./date.pl; $(CC) $(CFLAGS) -c ccx_2.18.c; $(FC)  -Wall -O2 -o $@ $(OCCXMAIN) ccx_2.18.a $(LIBS) -fopenmp
-
- ccx_2.18.a: $(OCCXF) $(OCCXC)
-diff --git a/ccx_2.18/src/cubtri.f b/ccx_2.18/src/cubtri.f
-index c13aa5b..831699f 100644
---- a/ccx_2.18/src/cubtri.f
-+++ b/ccx_2.18/src/cubtri.f
-@@ -77,7 +77,7 @@ C
-      &  mw,nfe
-       REAL*8 ALFA, ANS, ANSKP, AREA, EPS, ERR, ERRMAX, H, Q1, Q2, R1,R2,
-      * RDATA(1), D(2,4), S(4), T(2,3), VEC(2,3), W(6,NW), X(2),zero,
+ 
+-ccx_2.20: $(OCCXMAIN) ccx_2.20.a  $(LIBS)
++ccx_2.20: $(OCCXMAIN) ccx_2.20.a
+ 	./date.pl; $(CC) $(CFLAGS) -c ccx_2.20.c; $(FC)  -Wall -O2 -o $@ $(OCCXMAIN) ccx_2.20.a $(LIBS) -fopenmp
+ 
+ ccx_2.20.a: $(OCCXF) $(OCCXC)
+diff --git a/ccx_2.20/src/cubtri.f b/ccx_2.20/src/cubtri.f
+index caf27384..43f3a89c 100644
+--- a/ccx_2.20/src/cubtri.f
++++ b/ccx_2.20/src/cubtri.f
+@@ -80,7 +80,7 @@ ccc   change on 15.07.2022 from RDATA(1) to RDATA(*) to comply with recent
+ ccc   compiler versions
+      *     RDATA(*), D(2,4), S(4), T(2,3), VEC(2,3), W(6,NW), X(2),zero,
+ c     * RDATA(1), D(2,4), S(4), T(2,3), VEC(2,3), W(6,NW), X(2),zero,
 -     & point5,one,rnderr
 +     & point5,one,F,rnderr
  C       ACTUAL DIMENSION OF W IS (6,NW/6)
  C
        REAL*8 TANS, TERR, DZERO
-diff --git a/ccx_2.18/src/date.pl b/ccx_2.18/src/date.pl
-index 2b98376..f9184d2 100755
---- a/ccx_2.18/src/date.pl
-+++ b/ccx_2.18/src/date.pl
+diff --git a/ccx_2.20/src/date.pl b/ccx_2.20/src/date.pl
+index 82f01116..6a7d2fe7 100755
+--- a/ccx_2.20/src/date.pl
++++ b/ccx_2.20/src/date.pl
 @@ -13,7 +13,7 @@ while(<>){
-
- # inserting the date into ccx_2.18step.c
-
--@ARGV="ccx_2.18step.c";
+ 
+ # inserting the date into ccx_2.20step.c
+ 
+-@ARGV="ccx_2.20step.c";
 +@ARGV="CalculiXstep.c";
  $^I=".old";
  while(<>){
      s/You are using an executable made on.*/You are using an executable made on $date\\n");/g;
 @@ -30,5 +30,5 @@ while(<>){
  }
-
- system "rm -f ccx_2.18.c.old";
--system "rm -f ccx_2.18step.c.old";
+ 
+ system "rm -f ccx_2.20.c.old";
+-system "rm -f ccx_2.20step.c.old";
 +system "rm -f CalculiXstep.c.old";
  system "rm -f frd.c.old";
-diff --git a/ccx_2.18/src/premortar.c b/ccx_2.18/src/premortar.c
-index 62cd532..35905e9 100644
---- a/ccx_2.18/src/premortar.c
-+++ b/ccx_2.18/src/premortar.c
+diff --git a/ccx_2.20/src/premortar.c b/ccx_2.20/src/premortar.c
+index 3ce85129..bda58978 100644
+--- a/ccx_2.20/src/premortar.c
++++ b/ccx_2.20/src/premortar.c
 @@ -19,6 +19,7 @@
  #include <math.h>
  #include <stdlib.h>
@@ -197,54 +177,42 @@ index 62cd532..35905e9 100644
 +#include <string.h>
  #include "CalculiX.h"
  #include "mortar.h"
-
-diff --git a/ccx_2.18/src/resultsmech_us3.f b/ccx_2.18/src/resultsmech_us3.f
-index 1316f64..8e7d1a8 100644
---- a/ccx_2.18/src/resultsmech_us3.f
-+++ b/ccx_2.18/src/resultsmech_us3.f
-@@ -461,7 +461,7 @@
-      &     ihyper,istiff,elconloc,eth,kode,plicon,
-      &     nplicon,plkcon,nplkcon,npmat_,
-      &     plconloc,mi(1),dtime,k,
--     &     xstiff,alcon)
-+     &     xstiff,ncmat_)
-           e     = elas(1)
-           un    = elas(2)
-           rho   = rhcon(1,1,imat)
-diff --git a/ccx_2.18/src/sensi_coor.c b/ccx_2.18/src/sensi_coor.c
-index 8c61ea9..66a4927 100644
---- a/ccx_2.18/src/sensi_coor.c
-+++ b/ccx_2.18/src/sensi_coor.c
+ 
+diff --git a/ccx_2.20/src/resultsmech_us3.f b/ccx_2.20/src/resultsmech_us3.f
+index c76c509d..8d48220e 100644
+--- a/ccx_2.20/src/resultsmech_us3.f
++++ b/ccx_2.20/src/resultsmech_us3.f
+@@ -465,7 +465,7 @@
+      &     xstiff,ncmat_)
+           e     = elconloc(1)
+           un    = elconloc(2)
+-          rho   = rhcon(1,1,imat)        
++          rho   = rhcon(1,1,imat)
+           alp(1) = eth(1)!alcon(1,1,imat)    
+           alp(2) = eth(2)!alcon(1,1,imat)    
+           alp(3) = 0.d0 
+diff --git a/ccx_2.20/src/sensi_coor.c b/ccx_2.20/src/sensi_coor.c
+index 12ad8587..80089c22 100644
+--- a/ccx_2.20/src/sensi_coor.c
++++ b/ccx_2.20/src/sensi_coor.c
 @@ -18,6 +18,7 @@
  #include <stdio.h>
  #include <math.h>
  #include <stdlib.h>
 +#include <string.h>
  #include "CalculiX.h"
-
+ 
  void sensi_coor(double *co,ITG *nk,ITG **konp,ITG **ipkonp,char **lakonp,
-diff --git a/ccx_2.18/src/sensi_orien.c b/ccx_2.18/src/sensi_orien.c
-index f328ce1..36a5b4c 100644
---- a/ccx_2.18/src/sensi_orien.c
-+++ b/ccx_2.18/src/sensi_orien.c
+diff --git a/ccx_2.20/src/sensi_orien.c b/ccx_2.20/src/sensi_orien.c
+index b59af6f4..52568497 100644
+--- a/ccx_2.20/src/sensi_orien.c
++++ b/ccx_2.20/src/sensi_orien.c
 @@ -18,6 +18,7 @@
  #include <stdio.h>
  #include <math.h>
  #include <stdlib.h>
 +#include <string.h>
  #include "CalculiX.h"
-
+ 
  void sensi_orien(double *co,ITG *nk,ITG **konp,ITG **ipkonp,char **lakonp,
-diff --git a/ccx_2.18/src/us4_sub.f b/ccx_2.18/src/us4_sub.f
-index 7305d6b..36d77f7 100644
---- a/ccx_2.18/src/us4_sub.f
-+++ b/ccx_2.18/src/us4_sub.f
-@@ -454,7 +454,7 @@
-       REAL*8, INTENT(IN)  :: X(4,3),rho,h
-       REAL*8, INTENT(OUT) :: M(24,24)
-       REAL*8 :: ri,si,Nrs(4),dNr(4),dNs(4),Jm(2,2)
--      REAL*8 :: invJm,detJm,detinvJm,dNx(4),dNy(4),q1
-+      REAL*8 :: invJm(2,2),detJm,detinvJm,dNx(4),dNy(4),q1
-       REAL*8 :: m_3t(6,6), N_u(6,24),g_p(4,3)
-       INTEGER :: k,j
-       !
+
